2a3e257ab1769c12ebf297fc7a17ef62ccc1336e
Hugo Erazo <hugogilmar@gmail.com>
2024-09-02 02:33:37 -0600

    Router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,15 +5,30 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-import App from './App';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import BoardList from './components/BoardList';
+import Board from './components/Board';
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <BoardList />,
+  },
+  {
+    path: "/boards/:boardId",
+    element: <Board />,
+  }
+]);
 
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
